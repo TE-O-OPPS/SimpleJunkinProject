773ed034ae06f0d11317f18debf737250270fb85
TE-O-OPP <tekia2001@gmail.com>
2025-02-21 23:27:07 -0500

    run 15 times

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println(i);
             Thread.sleep(200);
         }
